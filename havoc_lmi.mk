689abdd828f3c2a2719ad101111a5f6dc4318a90
AbdullahPS <abdullahtabbal@github.com>
2021-02-28 15:04:47 +0200

    It's official time

--- havoc_lmi.mk
+++ havoc_lmi.mk
@@ -7,9 +7,13 @@
 $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
+
+HAVOC_BUILD_TYPE := Official
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_GAPPS_ARCH := arm64
 
+    
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := havoc_lmi
 PRODUCT_DEVICE := lmi
